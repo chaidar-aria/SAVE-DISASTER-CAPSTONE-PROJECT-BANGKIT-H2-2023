345f5cfdfb86aaa6bf7dc84e7af2349e3f1e0933
tprefersyouwithgrain <142720973+tprefersyouwithgrain@users.noreply.github.com>
2023-12-22 16:23:49 +0700

    Delete api-cluster/cluster_handler.py

--- api-cluster/cluster_handler.py
+++
@@ -1,7 +0,0 @@
-from model_loader import load_model
-
-def predict_cluster(nama_kota):
-    model = load_model()
-    # Lakukan prediksi kluster menggunakan model
-    kluster = model.predict([nama_kota])
-    return kluster[0]
